c841138f817911f6ef5c134e0d5485eeabb1c66b
Jean R. Mavi <jmavi21@yahoo.com>
2012-02-21 16:18:55 +0800

    Initial upload of v0.4, see CHANGELOG

--- Makefile
+++ Makefile
@@ -1,19 +1,51 @@
-REBAR=./rebar
-CT_DIR=../../logs
-REPORT_DIR=.
-DEPS_DIR=..
+APP := ct_surefire
 
-all: deps
-	${REBAR} compile
+ERL = `which erl`
+REBAR = `which rebar || echo "./rebar"`
+
+CT_DIR=../../logs
+OUTPUT_DIR=.
+
+.PHONY: test
+
+all:
+	@$(REBAR) get-deps
+	@$(REBAR) compile
+
+clean: distclean depsclean docsclean testclean
+
+compile:
+	@$(REBAR) compile
 
 deps:
-	${REBAR} get-deps
+	@$(REBAR) get-deps
+
+# these commands do a far better job at cleaning than rebar's clean command
+depsclean:
+	@rm -Rf deps
+
+# these commands do a far better job at cleaning than rebar's clean command
+distclean:
+	@rm -Rf ebin src/ct_suite_log_parser.erl
+
+docs:
+	@$(ERL) -noshell -run edoc_run application '$(APP)' '"."' '[]'
+
+# these commands do a far better job at cleaning than rebar's clean command
+docsclean:
+	@rm -Rf doc
 
 #simple but sufficient for now
 test:
-	${MAKE}	xmlify CT_DIR=test/t001 REPORT_DIR=test DEPS_DIR=deps
+	@$(MAKE) xmlify CT_DIR=test/t001 OUTPUT_DIR=test
 
+# these commands do a far better job at cleaning than rebar's clean command
+testclean:
+	@rm -Rf test/*.xml
+
+# Add the following to the code path
+#     ebin - self-explanatory
+#     deps/*/ebin - when used as a stand-alone
+#     ../*/ebin - when used as a rebar dependency
 xmlify:
-	erl -noshell -pa ebin ${DEPS_DIR}/*/ebin -run ct_surefire to_surefire_xml ${CT_DIR} ${REPORT_DIR} -s init stop
-
-.PHONY: test
+	@$(ERL) -noshell -pa ebin deps/*/ebin ../*/ebin -run ct_surefire generate_surefire_reports ${CT_DIR} ${OUTPUT_DIR} -s init stop
--- src/ct_surefire.erl
+++ src/ct_surefire.erl
@@ -1,203 +1,168 @@
-%%
+%%----------------------------------------------------------------------------------------------------------------------------------------
 %% ct_surefire
 %%
 %% Copyright (c) 2011 Martin Scholl (ms@funkpopes.org)
 %% Copyright (c) 2011 global infinipool GmbH
 %%
-%% Permission is hereby granted, free of charge, to any person obtaining a copy
-%% of this software and associated documentation files (the "Software"), to deal
-%% in the Software without restriction, including without limitation the rights
-%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%% copies of the Software, and to permit persons to whom the Software is
-%% furnished to do so, subject to the following conditions:
+%% Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+%% (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+%% publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+%% subject to the following conditions:
 %%
-%% The above copyright notice and this permission notice shall be included in
-%% all copies or substantial portions of the Software.
+%% The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 %%
-%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-%% THE SOFTWARE.
-%% -------------------------------------------------------------------
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+%% ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+%% THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%%----------------------------------------------------------------------------------------------------------------------------------------
+%% @author Martin Scholl <ms@funkpopes.org>
+%% @author Jean R. Mavi <jmavi21@yahoo.com>
+%% @doc
+%%     A utility Erlang library that converts Common Test's suite.log.html output file(s) into a Surefire XML report
+%% @end
+%% @copyright
+%%     2011 Martin Scholl, 2011 global infinipool GmbH
+%% @end
+%% @type error_tuple() = {error, Reason :: term()}.
+%% @type ok_tuple() = {ok, Value :: term()}.
+%%----------------------------------------------------------------------------------------------------------------------------------------
 -module(ct_surefire).
+-compile([{parse_transform, do}]).
 
-%% API
--export([
-	 to_surefire_xml/1,
-	 to_surefire_xml/2
-	]).
+%% API (v0.3)
+-export([to_surefire_xml/1, to_surefire_xml/2]).
+%% API (v0.4)
+-export([generate_surefire_reports/1, generate_surefire_reports/2]).
 
--define(TAG(Name), ?TAG(Name, _, _)).
--define(TAG(Name, Childs), ?TAG(Name, _, Childs)).
--define(TAG(Name, Args, Childs), {<<??Name>>, Args, Childs}).
+%%========================================================================================================================================
+%% API (version 0.3)
+%%========================================================================================================================================
+%% @spec to_surefire_xml(Args :: list()) -> ok | error_tuple()
+%% @equiv generate_surefire_reports(hd(Args), hd(tl(Args)))
+%% @doc This function is called when running ct_surefire from the command line using the -run option
+to_surefire_xml([CtLogDir, OutputDir]) ->
+    generate_surefire_reports(CtLogDir, OutputDir);
 
--record(res, {test_id,suite,case_id,duration,result,error}).
+to_surefire_xml(_) ->
+    error(badarg).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
-to_surefire_xml([CtDir, OutputDir]) when is_list(CtDir), 
-					 is_list(OutputDir) ->
-    to_surefire_xml(CtDir, OutputDir).
+%% @spec to_surefire_xml(CtLogDir :: string(), OutputDir :: string()) -> ok | error_tuple()
+%% @equiv generate_surefire_reports(CtLogDir, OutputDir)
+to_surefire_xml(CtLogDir, OutputDir) ->
+    generate_surefire_reports(CtLogDir, OutputDir).
 
-%% computes a surefire XML report from a CT directory
-%% @param CtDir directory where CT logs are stored
-%% @param OutputDir Directory where XML report files will be stored
-to_surefire_xml(CtDir, OutputDir) ->
-    RunDir = find_latest_rundir(CtDir),
-    Reports = available_reports(RunDir),
-    [begin
-	 ReportFilename = "TEST-" ++ App ++ ".xml",
-	 OutputFilename = filename:join([OutputDir, ReportFilename]),
-	 write_report(ReportDir, App, OutputFilename)
-     end || {App, ReportDir} <- Reports],
+%%========================================================================================================================================
+%% API (version 0.4)
+%%========================================================================================================================================
+%% @spec generate_surefire_reports(Args :: list()) -> ok | error_tuple()
+%% @equiv generate_surefire_reports(CtLogDir, OutputDir)
+%% @doc This function is called when running ct_surefire from the command line using the -run option
+generate_surefire_reports([CtLogDir, OutputDir]) ->
+    generate_surefire_reports(CtLogDir, OutputDir);
+
+generate_surefire_reports(_) ->
+    error(badarg).
+
+%% @spec generate_surefire_reports(CtLogDir :: string(), OutputDir :: string()) -> ok | error_tuple()
+%% @doc Parses and translates Common Test HTML logs from the log directory then writes the corresponding Surefire XML report in the output directory
+generate_surefire_reports(CtLogDir, OutputDir) ->
+    do([error_m ||
+        validate_directory(CtLogDir),
+        validate_directory(OutputDir),
+        HtmlLogFiles <- get_latest_reports(CtLogDir),
+        ParsedReports <- parse_reports(CtLogDir, HtmlLogFiles),
+        write_reports(OutputDir, ParsedReports)
+    ]).
+
+%%========================================================================================================================================
+%% Internal functions (reports)
+%%========================================================================================================================================
+%% @spec get_latest_reports(CtLogDir :: string()) -> ok_tuple() | error_tuple()
+%% @doc Given the Common Test log directory as the base directory, return a list of relative paths to the most recent suite.log.html files
+get_latest_reports(CtLogDir) ->
+    % used just a single filelib:wildcard/2 call here so we don't have to do a validate_directory/1 or file_lib:is_dir/1 multiple times
+    HtmlLogFiles = filelib:wildcard(filename:join(["ct_run.*", "*.logs", "run.*", "suite.log.html"]), CtLogDir),
+
+    case lists:reverse(lists:sort(HtmlLogFiles)) of
+        [] ->
+            {error, no_test_case_dirs_found};
+        [LatestHtmlLogFile | _] ->
+            [LatestTestRunDir, _, _, "suite.log.html"] = filename:split(LatestHtmlLogFile),
+            {ok, [LogFile || LogFile <- HtmlLogFiles, LatestTestRunDir =:= hd(filename:split(LogFile))]}
+    end.
+
+%% @spec parse_reports(CtLogDir :: string(), LogFiles :: [string()]) -> ok_tuple() | error_tuple()
+%% @doc Given the Common Test log directory as the base directory and a list of relative paths to the most recent suite.log.html files,
+%%     returns the successfully-parsed representations of the Common Test results
+parse_reports(CtLogDir, LogFiles) ->
+    ParsedReports = lists:foldl(fun(LogFile, Results) ->
+        [_, TestRunDir, _, "suite.log.html"] = filename:split(LogFile),
+        [_, AppName | _] = string:tokens(TestRunDir, "."),
+
+        case ct_suite_log_parser:file(filename:join([CtLogDir, LogFile])) of
+            {ok, {Elapsed, Failed, Skipped, TestCases}} ->
+                [{AppName, Elapsed, Failed, Skipped, TestCases} | Results];
+            _ ->
+                Results
+        end
+    end, [], LogFiles),
+
+    case ParsedReports of
+        [] ->
+            {error, ct_report_not_found};
+        _ ->
+            {ok, ParsedReports}
+    end.
+
+%% @spec write_reports(CtLogDir :: string(), [report()]) -> ok
+%% @doc Given the Output directory and the list of Common Test results, writes the corresponding Surefire XML report(s)
+write_reports(OutputDir, [{AppName, Elapsed, Failed, Skipped, TestCases} | Reports]) ->
+    OutputFile = filename:join([OutputDir, "TEST-" ++ AppName ++ "_ct.xml"]),
+
+    XmlHeader = io_lib:format("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>~n<testsuite tests=\"~p\" failures=\"~p\" errors=\"0\" skipped=\"~p\" time=\"~s\" name=\"common_test '~s'\">~n", [
+		length(TestCases), Failed, Skipped, Elapsed, AppName
+	]),
+
+    XmlBody = lists:map(fun({TestCaseId, TestSuiteModule, TestCaseFunction, Duration, ok}) ->
+        io_lib:format("    <testcase name=\"~s:~s/0_~s\" time=\"~s\" />~n", [
+            TestSuiteModule, TestCaseFunction, TestCaseId, Duration
+        ]);
+    ({TestCaseId, TestSuiteModule, TestCaseFunction, Duration, {failed, Reason}}) ->
+        io_lib:format("    <testcase name=\"~s.~s/0_~s\" time=\"~s\">~n        <error type=\"error\">~s</error>~n        <system-out />~n    </testcase>~n", [
+            TestSuiteModule, TestCaseFunction, TestCaseId, Duration, Reason
+        ]);
+    ({TestCaseId, TestSuiteModule, TestCaseFunction, Duration, {skipped, _Reason}}) ->
+        io_lib:format("    <testcase name=\"~s:~s/0_~s\" time=\"~s\" >~n        <skipped />~n    </testcase>", [
+            TestSuiteModule, TestCaseFunction, TestCaseId, Duration
+        ])
+    end, TestCases),
+
+    XmlFooter = "</testsuite>",
+
+    file:write_file(OutputFile, XmlHeader ++ XmlBody ++ XmlFooter),
+
+    write_reports(OutputDir, Reports);
+
+write_reports(_, []) ->
     ok.
 
+%%========================================================================================================================================
+%% Internal functions (validation)
+%%========================================================================================================================================
+%% @spec validate_directory(Directory :: term()) -> ok | error_tuple()
+%% @doc If Directory is a string reference to an actual directory, returns ok; otherwise, returns an error tuple
+validate_directory(Directory) when false =:= is_list(Directory); [] =:= Directory ->
+    {error, {invalid_directory, [
+        {directory, Directory}
+    ]}};
 
-find_latest_rundir(Dir) ->
-    Dirs = filelib:wildcard(filename:join([Dir, "ct_run.*"])),
-    hd(lists:reverse(lists:sort(Dirs))).
-
-available_reports(Dir) ->
-    Apps = [string:substr(A, 6) || A <- filelib:wildcard("apps.*", Dir)],
-    Dirs = filelib:wildcard(filename:join([Dir, "apps.*", "run.201*"])),
-    lists:zip(Apps, Dirs).
-
-write_report(ReportDir, App, OutputFilename) ->
-    LogFilename = filename:join(ReportDir, "suite.log.html"),
-    Results = parse_results(LogFilename),
-    Xml = results_to_xml(App, Results),
-    ok = file:write_file(OutputFilename, Xml).
-    
-    
-
-parse_results(Filename) ->
-    {ok, RawHTML} = file:read_file(Filename),
-    case mochiweb_html:parse(RawHTML) of
-	?TAG(html, [], [?TAG(head), ?TAG(body, _, TestResults)]) ->
-	    DescendPath = [<<"p">>,<<"p">>,<<"p">>,<<"p">>,<<"table">>],
-	    TestTab = follow(DescendPath, TestResults),
-	    filter_tests(TestTab);
-	_ ->
-	    error(unsupported_html, Filename)
+validate_directory(Directory) ->
+    case filelib:is_dir(Directory) of
+        true ->
+            ok;
+        _ ->
+            {error, {invalid_directory, [
+                {directory, Directory}
+            ]}}
     end.
-
-results_to_xml(App, Results) ->
-    XmlHdr = io_lib:format(
-	       "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
-	       "<testsuite tests=\"~b\" failures=\"0\" errors=\"~b\" skipped=\"~b\" time=\"~f\" name=\"ct.~s'\">", 
-	       [
-		length(Results),
-		count_by_result(failed, Results),
-		count_by_result(skipped, Results),
-		sum_duration(Results),
-		App
-	       ]),
-    Testcases = [xmlify_result(R) || R <- Results],
-    XmlFooter = "</testsuite>",
-    
-    [XmlHdr, Testcases, XmlFooter].
-	    
-    
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-xmlify_result(#res{result = ok, suite = Suite, case_id = Case,
-		   test_id = Id, duration = D}) ->
-    io_lib:format(" <testcase name=\"~s.~s.~s\" time=\"~s\" />~n",
-		  [Suite,Case,Id,D]);
-xmlify_result(#res{error = Error, suite = Suite, case_id = Case,
-		   test_id = Id, duration = D}) ->
-    io_lib:format(" <testcase name=\"~s.~s.~s\" time=\"~s\">~n"
-		  "  <error type=\"error\">~s</error>~n"
-		  "  <system-out></system-out>~n"
-		  " </testcase>~n",
-		  [Suite,Case,Id,D,Error]).
-
-
-follow([], HTML) ->
-    HTML;
-follow([Key | Rest], HTML) ->
-    {value, {Key, _, Childs}} = lists:keysearch(Key, 1, HTML),
-    follow(Rest, Childs).
-
-filter_tests(HTML) ->
-    lists:reverse(lists:foldl(fun fold_row/2, [], HTML)).
-
-fold_row(Row, Result) ->
-    case parse_row(Row) of
-	false ->
-	    Result;
-	Res ->
-	    [Res | Result]
-    end.
-
-parse_row(Row) ->
-    case Row of
-	?TAG(tr, _,
-	     [
-	      ?TAG(td, [?TAG(font, _, [TestNum])]),
-	      ?TAG(td, [?TAG(font, _, [Suite])]),
-	      ?TAG(td, [{_, _, [Case]}]),
-	      ?TAG(td), %% log
-	      ?TAG(td, [?TAG(font, _, [Time])]),
-	      ?TAG(td, [?TAG(font, _, [RawResult])]),
-	      ?TAG(td, RawError)
-	     ]) ->
-	    Error = iolist_to_binary(error_to_text(RawError)),
-	    Duration = parse_duration(Time),
-	    Result = to_result(RawResult),
-	    {res, TestNum, Suite, Case, Duration, Result, Error};
-	?TAG(tr, _,
-	     [
-	      ?TAG(td, [?TAG(font, _, [TestNum])]),
-	      ?TAG(td, [?TAG(font, _, [Suite])]),
-	      ?TAG(td, [{_, _, [Case]}]),
-	      ?TAG(td), %% log
-	      ?TAG(td, [?TAG(font, _, [Time])]),
-	      ?TAG(td, [?TAG(font, _, [<<"Ok">>])])
-	     ]) ->	
-	    {res, TestNum, Suite, Case, parse_duration(Time),
-	     ok, <<>>};
-	_ ->
-	    false
-    end.
-	
-to_result(<<"SKIPPED">>) ->
-    skipped;
-to_result(<<"FAILED">>) ->
-    failed.
-
-error_to_text(B) when is_binary(B) ->
-    B;
-error_to_text({_, _, Text}) ->
-    error_to_text(Text);
-error_to_text(HtmlError) when is_list(HtmlError) ->
-    lists:map(fun error_to_text/1, HtmlError).
-
-count_by_result(Status, Res) ->
-    Filtered = lists:filter(
-		 fun (#res{result=S}) -> S == Status end,
-		 Res),
-    length(Filtered).
-
-sum_duration(Res) ->
-    Durations = lists:map(
-		  fun(#res{duration=D}) ->
-			  list_to_float(binary_to_list(D))
-		  end,
-		  Res),
-    lists:foldl(fun erlang:'+'/2, 0.0, Durations).				 
-
-% parallel testcases
-parse_duration(?TAG(i, [D])) ->
-    parse_duration(D);
-parse_duration(<<"died">>) -> <<"0">>;
-parse_duration(D) ->
-    BS = byte_size(D)-1,
-    <<Dura:BS/binary,"s">> = D,
-    Dura.
